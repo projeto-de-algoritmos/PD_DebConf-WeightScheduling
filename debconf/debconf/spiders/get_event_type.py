f17c561b6d8e91e28ec62fb46f20db3daf196c73
Thais <rotinasoftware@gmail.com>
2023-12-11 21:26:40 -0300

    Salvando os tipos no json

--- debconf/debconf/spiders/get_event_type.py
+++ debconf/debconf/spiders/get_event_type.py
@@ -18,10 +18,26 @@ class EventTypeSpider(scrapy.Spider):
                 i = i + 1
         
         for link in find_links:
-            print(link)
-            print("----")
             url = f"https://debconf23.debconf.org{link}"
-            return scrapy.Request(url, callback=self.parse_dir_contents)
+            yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+    def create_dict(self, title, event_type):
+        event_catalogue = {
+            'titulo': title,
+            'tipo': event_type,
+        }
+        return self.save_json(event_catalogue)
+
+    def save_json(self, catalogue):
+        file_path = '../events.json'
+        try:
+            with open(file_path, 'r') as fp:
+                existing_data = json.load(fp)
+        except (json.JSONDecodeError, FileNotFoundError):
+            existing_data = {}
+        existing_data.update(catalogue)
+        with open(file_path, 'a') as fp:
+            json.dump(existing_data, fp, indent=4)
 
     def parse_dir_contents(self, response):
         title = response.xpath('//h1/text()').extract()
@@ -29,3 +45,5 @@ class EventTypeSpider(scrapy.Spider):
 
         event_type = response.xpath('//div/p/text()').re('Type:\s*(.+)')
         event_type = event_type[0]
+        
+        return self.create_dict(title, event_type)
