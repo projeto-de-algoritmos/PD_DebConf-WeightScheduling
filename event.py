e8fcd76fa53df898cba24523d04d2657f672d7bd
Thais <rotinasoftware@gmail.com>
2023-12-11 23:14:56 -0300

    Tratamento dos pesos

--- event.py
+++ event.py
@@ -1,27 +1,28 @@
 import json
 
 class Event:
-    def __init__(self, titulo, horario_inicio, horario_fim, valor):
+    def __init__(self, titulo, horario_inicio, horario_fim, tipo, valor):
 
         self.titulo = titulo
         self.horario_inicio = horario_inicio
         self.horario_fim = horario_fim
+        self.tipo = tipo
         self.valor = valor
-
 
     def as_dict(self):
         return {
             'titulo': self.titulo,
             'horario_inicio': self.horario_inicio,
             'horario_fim': self.horario_fim,
-            'valor': self.valor
+            'tipo': self.tipo,
+            'valor': self.valor,
         }
 
 def sort_by_finish_time(agenda):
     # Ordena eventos por ordem de término e transforma em dicionário
     event_objects = []
     for i in agenda:
-        event_objects.append(Event(i['titulo'], i['horario_inicio'], i['horario_fim'], i['valor']))
+        event_objects.append(Event(i['titulo'], i['horario_inicio'], i['horario_fim'], i['tipo'], i['valor']))
 
     sorted_events = sorted(event_objects, key=lambda event: event.horario_fim)
     sorted_events_as_dicts = [event.as_dict() for event in sorted_events]
@@ -53,11 +54,32 @@ def find_best_schedule(agenda):
             i -= 2
         else:
             i -= 1
-
     return list(reversed(best_programming))
 
 def get_data(day):
     with open('./schedule.json', 'r') as fp:
         data_json = json.load(fp)
-    print(find_best_schedule(data_json[day]))
-    return find_best_schedule(data_json[day])
+    return data_json[day]
+    
+def get_weight():
+    with open('./events.json', 'r') as fp:
+        data_type = json.load(fp)
+    return data_type
+
+def include_weight(day):
+    data_day = get_data(day)
+    data_type = get_weight()
+    for data in data_day:
+        title = data['titulo']
+        for types in data_type:
+            if types['titulo'] == title:
+                data['tipo'] = types['tipo']
+    return data_day
+
+def transform_type_in_value(data_with_weight, weight_event_type):
+    print(data_with_weight)
+    print(weight_event_type)
+    for data in data_with_weight:
+        data['valor'] = weight_event_type[data['tipo']]
+    return data_with_weight
+
